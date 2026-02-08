81c51e188e5b00fcc6b331ea953a83795ab7ffa9
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-09 00:28:16 +0530

    Understand Types Of Operators

---
+++ code-4.py
@@ -0,0 +1,63 @@
+# Arithmetic Operators +, -, *, /
+a = 6
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Assignment Operators =, +=, -=, *=, /=
+c = 5-2 # Assign 5-2 in c
+print(c)
+d = 7
+d += 3 # Increment the value of d by 3 and then assign it to d
+print(d)
+
+e = 9
+e -= 4 # Decrement the value of e by 4 and then assign it to e
+print(e)
+
+f = 5
+f *= 4
+print(f)
+
+g = 9
+g /= 3 
+print(g)
+
+# Comparison Operators ==, >, <, >=, <=, != (Gives Result in Boolean)
+h = 5==5
+print(h)
+
+h = 5>=5
+print(h)
+
+h = 5<=5
+print(h)
+
+h = 5!=5
+print(h)
+
+# Logical Operators AND, OR, NOT
+k = True and True
+print(k)
+k = True and False
+print(k)
+k = False and True
+print(k)
+k = False and False
+print(k)
+
+m = True or True
+print(m)
+m = True or False
+print(m)
+m = False or True
+print(m)
+m = False or False
+print(m)
+
+p =  not True
+print(p)
+p = not False
+print(p)
