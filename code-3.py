a240f91b42639b4014c4b4e92662567d6fede485
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-08 23:35:42 +0530

    Understand Variables and Data types

---
+++ code-3.py
@@ -0,0 +1,10 @@
+a = 1 # a is an Integer.
+b = 5.234 # b is a Floating point number.
+c = "Purnami" # c ia a String.
+d = True # d is a Boolean variable.
+e = None # e is a None type variable.
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+print (type(e))
