beb5e690490f9b02f50b25e4f57e5b13fffc8950
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-09 23:50:13 +0530

    Understand Type Conversion

---
+++ code-5.py
@@ -0,0 +1,17 @@
+a = 56.98
+print(type(a)) # class<'float'>
+
+a = str(56.98)
+print(type(a)) # class<'str'>
+
+a = int(56.98)
+print(type(a)) # class<'int'>
+
+b = "Shinchan"
+print(type(b)) # class<'str'>
+
+c = "75"
+print(type(c)) # class<'str'>
+
+d = 45
+print(type(d)) # class<'int'>
