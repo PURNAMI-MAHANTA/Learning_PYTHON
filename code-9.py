8f639820004572fca82c1dfd425f282bdc274d35
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-14 00:17:37 +0530

    Learn about some string functions

---
+++ code-9.py
@@ -0,0 +1,15 @@
+word = "Demon slaYer"
+
+print(word) #Demon slaYer
+print(len(word)) #12
+print(word.upper()) #DEMON SLAYER
+print(word.lower()) #demon slayer
+print(word.title()) #Demon Slayer
+print(word.capitalize()) #Demon slayer
+
+name = " Luffy "
+
+print(name.strip()) #Luffy
+print(name.lstrip()) #Luffy
+print(name.rstrip()) # Luffy
+
