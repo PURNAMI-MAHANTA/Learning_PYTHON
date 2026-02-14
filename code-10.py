1eb5899b771ea0a63fd59bc8240f879287574dde
PURNAMI-MAHANTA <pareshkumohanta7@gmail.com>
2026-02-14 23:35:26 +0530

    Learn to use some escape sequence characters

---
+++ code-10.py
@@ -0,0 +1,15 @@
+# Some Escape sequence characters \n-->new line, \t-->tab space, \r-->Carriage return, \v-->vertical tab, \"-->Double Quote, \'-->Single Quote
+anime = "I love to watch anime\nCurrently I am watching \"One Piece\""
+print(anime)
+
+skill = "I have started to learn a new skill\t\"PYTHON\""
+print(skill)
+
+welcome = "Welcome\nIt\'s my github profile"
+print(welcome)
+
+new = "Learning\vnew\vskills"
+print(new)
+
+code = "Love\rcoding"
+print(code)
